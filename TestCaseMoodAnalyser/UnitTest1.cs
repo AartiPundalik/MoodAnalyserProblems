0b2bad0fe8417a37770bc2a4f02c719fb0dcf3d9
AartiPundalik <aartipundalik68@gmail.com>
2023-03-05 22:49:31 +0530

    [Aarti] Add : UC2 - Analyze And Respond Happy Mood

--- TestCaseMoodAnalyser/UnitTest1.cs
+++ TestCaseMoodAnalyser/UnitTest1.cs
@@ -8,9 +8,15 @@ namespace TestCaseMoodAnalyser
         [Test]
         public void GivenSadMood_WhenAnalyzed_ShouldReturnsad()
         {
-
             string result = abilitytoAnalyze.AnalyseMood("I Am In sad Mood");
             Assert.AreEqual(result, "sad");
         }
+        [Test]
+        public void HappyMood_WhenAnalyse_ShouldReturnSad()
+        {
+            string result = abilitytoAnalyze.AnalyseMood("I am in happy mood");
+            Assert.AreEqual(result, "happy");
+        }
     }
 }
+    
